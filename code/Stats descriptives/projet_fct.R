c0d88e23b96d909fda11a0412aa07d1df254c8ea
Kdesantiago <kylliann.desantiago@hotmail.fr>
2021-02-06 15:19:29 +0100

    start of the visualization part

--- code/Stats descriptives/projet_fct.R
+++ code/Stats descriptives/projet_fct.R
@@ -8,8 +8,8 @@ conversion_nucleotide=function(V){
 
 #data_filtered[data_filtered$Covariable=="nucléotide",noms]=apply(data_filtered[data_filtered$Covariable=="nucléotide",noms],FUN = conversion_nucleotide,MARGIN = 2)
 
-extraction_vect=function(clef){
-  tamp=(data_filtered[data_filtered[,2]==clef,])
+extraction_vect=function(df,clef){
+  tamp=(df[df[,2]==clef,])
   tamp= tamp %>% dplyr::select(-c(Position, Covariable))
   m = as.matrix(tamp)
   res = c()
