fb86e2c686f36ea9caff5ca7f6fc5da3115bdc9c
Kdesantiago <kylliann.desantiago@hotmail.fr>
2021-02-06 14:14:55 +0100

    Update

--- code/projet_fct.R
+++ code/projet_fct.R
@@ -7,3 +7,15 @@ conversion_nucleotide=function(V){
 }
 
 #data_filtered[data_filtered$Covariable=="nucléotide",noms]=apply(data_filtered[data_filtered$Covariable=="nucléotide",noms],FUN = conversion_nucleotide,MARGIN = 2)
+
+extraction_vect=function(clef){
+  tamp=(data_filtered[data_filtered[,2]==clef,])
+  tamp= tamp %>% dplyr::select(-c(Position, Covariable))
+  m = as.matrix(tamp)
+  res = c()
+  for (i in seq(1:nrow(m))){
+    res = c(res, m[i,])
+  }
+  names(res)=NULL
+  return(res)
+}
