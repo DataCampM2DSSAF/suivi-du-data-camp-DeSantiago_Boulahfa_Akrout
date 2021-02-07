c7ea47868760cd1e93b1c1922aa3a2e862e88cd1
Kdesantiago <kylliann.desantiago@hotmail.fr>
2021-02-07 16:16:55 +0100

    Update

--- code/projet_fct.R
+++ code/projet_fct.R
@@ -1,3 +1,5 @@
+############ Visualisation ############ 
+
 conversion_nucleotide=function(V){
   V=case_when(
     V==3 ~ "G", 
@@ -22,3 +24,22 @@ extraction_vect=function(df,clef){
 # Permet de récupérer toutes les valeurs 
 # pour tout le monde et toutes les nucléotides de ce que l'on souhaite.
 # Ex: La liste de tous les nucléotides.
+
+
+############ Modèles simples ############ 
+
+
+train_test_split=function(data,proportion=0.8){
+  N=dim(data)[1]
+  idx=sample.int(N/68)
+  nb_individu_prop=floor(N/68*proportion)
+  
+  res=rep(0,nb_individu_prop*68)
+  for (i in 1:floor(nb_individu_prop)){
+    res[((i-1)*68+1):(i*68)]=seq((idx[i]*68+1),(idx[i]*68)+68)
+  }
+  train=data[res,]
+  val=data[-res,]
+  return(list("train"=train,"val"=val))
+  
+}
